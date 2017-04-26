a4a83693ea981576b7ca34622ec650d47e81319a
COLD-PLAY <mi_xi_shi_zi@163.com>
2017-04-26 23:53:50 +0800

    modify MySQLHandle.php and hello.php

--- MySQLHandle.php
+++ MySQLHandle.php
@@ -5,6 +5,45 @@
 	<meta charset="utf-8">
 </head>
 <body>
+	<?php
+	$servername = 'localhost';
+	$username = 'root';
+	$password = 'liaozhou1998';
+	$dbname = 'MYDBPDO';
+
+	//////////////////////////////// create a database named MYDBPDO
+	// try {
+	// 	$conn = new PDO("mysql:host=$servername;dbname=COLDPLAY", $username, $password);
+	// 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// 	$sql = 'CREATE DATABASE MYDBPDO';
+	// 	$conn->exec($sql);
+	// 	// echo 'Connected successfully';
+	// 	echo 'Database created successfully';
+	// } catch (PDOException $e) {
+	// 	echo $e->getMessage();
+	// }
+
+	//////////////////////////////// create a table named MYGUESTS
+	// try {
+	// 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	// 	// set the PDO error mode to exception
+	// 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// 	// sql to create table
+	// 	$sql = 'CREATE TABLE MYGUESTS(
+	// 	ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	// 	FIRSTNAME VARCHAR(30) NOT NULL,
+	// 	LASTNAME VARCHAR(30) NOT NULL,
+	// 	EMAIL VARCHAR(50),
+	// 	REG_DATE TIMESTAMP)';
+
+	// 	// use exec() because no results are returned
+	// 	$conn->exec($sql);
+	// 	echo 'Table MYGUESTS created successfully';
+	// } catch (PDOException $e) {
+	// 	echo $e->getMessage();
+	// }
+
 	
+	?>	
 </body>
 </html>
--- hello.php
+++ hello.php
@@ -1,9 +1,15 @@
 <?php
-echo $_POST["name"];
-echo "<br>";
-echo $_POST["email"];
+// echo $_POST["name"];
+// echo "<br>";
+// echo $_POST["email"];
 
 // echo $_GET["name"];
 // echo "<br>";
 // echo $_GET["email"];
+// echo 'hello sublime text3'
+$para = array();
+$para[0] = 'liaozhou';
+$para[1] = 'zhoujielun';
+
+echo count($para);
 ?>
